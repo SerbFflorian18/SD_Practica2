77bbaf505229dae8ab11ff8868f056c4c9cc17a2
Mohamed-Benaya <mohamed.benaya@estudiants.urv.cat>
2024-05-11 20:47:16 +0200

    Merge

---
+++ SD-Task2-2024-base-code_V1.1/SD-Task2-2024-base-code/centeralized/storage_service.py
@@ -0,0 +1,47 @@
+import pickle
+
+
+class StorageService:
+    def __init__(self):
+        self.database = dict()
+        try:
+            self.restore()
+        except FileNotFoundError:
+            pass
+
+    # Save key and value in the database
+    def save(self, key, value):
+        try:
+            self.database[key] = value
+            self.dump()
+            success = True
+        except:
+            success = False
+        
+        return success
+
+    # Get the value of the key
+    def fetch(self, key):
+
+        try:
+            data = self.database[key]
+            found = True
+        except:
+            data = None
+            found = False
+
+        return found, data
+
+    # Save The data in a file
+    def dump(self):
+        with open("storage.pickle", "wb") as f:
+            pickle.dump(self.database, f)
+
+    # Read data from the file
+    def restore(self):
+        with open("storage.pickle", "rb") as f:
+            loaded = pickle.load(f)
+            self.database  = loaded
+
+
+
---
+++ SD-Task2-2024-base-code_V1.1/SD-Task2-2024-base-code/centeralized/test_client.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../proto')
+
+import grpc
+import store_pb2_grpc
+import store_pb2
+
+
+# Open a gRPC channel
+channel = grpc.insecure_channel('localhost:32770')
+
+# Create a stub (client)
+stub = store_pb2_grpc.KeyValueStoreStub(channel)
+
+request = store_pb2.PutRequest(key='first', value='newer')
+response = stub.put(request)
+print("res: ", response.success)
+
+try:
+    #slow_request = store_pb2.SlowDownRequest(seconds=2)
+    #slow_response = stub.slowDown(slow_request)
+
+    #restore_request = store_pb2.RestoreRequest()
+    #restore_response = stub.restore(restore_request)
+
+    get_request = store_pb2.GetRequest(key='first')
+    get_response = stub.get(get_request)
+    print(get_response.value)
+
+except grpc.RpcError as e:
+    print("Timeout")
